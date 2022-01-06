3c021f21c6aced8835a808a9ff4666cbc4151a2a
40765 <40765@DESKTOP-MJFTUAP>
2022-01-06 17:28:10 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Negrea_Daniel_Project
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
